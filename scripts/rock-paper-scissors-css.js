8b03bb06716ed79e041c5e1a94912c08922454d9
Iulian373 <105042401+Iulian373@users.noreply.github.com>
2023-07-21 11:40:11 +0300

    Update rock-paper-scissors-css.js

--- scripts/rock-paper-scissors-css.js
+++ scripts/rock-paper-scissors-css.js
@@ -43,6 +43,19 @@ function autoPlay() {
     }
 }
 
+document.querySelector('.js-rock-button')
+    .addEventListener('click', () => {
+        playGame('rock');
+    })
+document.querySelector('.js-paper-button')
+    .addEventListener('click', () => {
+        playGame('paper');
+    })
+document.querySelector('.js-scissors-button')
+    .addEventListener('click', () => {
+        playGame('scissors');
+    })
+
 function playGame (playerChoise) {
     const computerChoice = pickComputerMove();
 
